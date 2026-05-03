ea607699b3288ae180c55da6c753010614b640a5
Sheng Xia <652024330032@smail.nju.edu.cn>
2026-05-04 07:16:05 +0800

    internvideo修复

--- vlmeval/vlm/internvideo2_5.py
+++ vlmeval/vlm/internvideo2_5.py
@@ -8,7 +8,7 @@ from PIL import Image
 from torchvision.transforms.functional import InterpolationMode
 from transformers import AutoModel, AutoTokenizer
 
-from ..base import BaseModel
+from .base import BaseModel
 
 IMAGENET_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_STD = (0.229, 0.224, 0.225)
